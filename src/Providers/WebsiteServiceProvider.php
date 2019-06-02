119cead7b03e76c4d7de6ab1b9177e933ed3a966
erniesto77 <docplanetasite@gmail.com>
2019-06-03 03:54:15 +0500

    init

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace O2Web\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+
+
+    public function boot()
+    {
+        dd('we are loading our package service provider...');
+    }
+
+    public function register()
+    {
+        # code...
+    }
+
+}
